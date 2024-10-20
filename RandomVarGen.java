9b4f633238ffd70abaf4d705fefb594905f203f8
NikithaPrabhu31 <144188180+NikithaPrabhu31@users.noreply.github.com>
2024-10-20 11:07:06 -0400

    Update RandomVarGen.java

--- RandomVarGen.java
+++ RandomVarGen.java
@@ -1,3 +1,14 @@
-public class RandomVarGen{
-  //do stuff
+public class RandomVarGen {
+    private RandomNumGen randomnum;
+
+    public RandomVarGen(RandomNumGen randomnum) {
+        this.randomnum = randomnum;
+    }
+
+    public double ContinuousX() {
+        double u = randomnum.nextRandom();
+        return Math.pow(8 * u, 2.0 / 3.0); //inverse of the CDF
+    }
+
+    
 }
