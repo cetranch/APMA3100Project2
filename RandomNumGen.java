8dcd8005ffa55cbaa949fda0366824560dd12d5f
NikithaPrabhu31 <144188180+NikithaPrabhu31@users.noreply.github.com>
2024-10-20 10:00:13 -0400

    Update RandomNumGen.java

--- RandomNumGen.java
+++ RandomNumGen.java
@@ -1,4 +1,22 @@
-public class RandomNumGen{
-//do stuff
-  
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RandomNumGen {
+    private static final int seed = 1000; //starting value seed
+    private static final int multiplier = 24693; //multiplier a
+    private static final int increment = 3517; // increment c
+    private static final int modulus = (int) Math.pow(2, 17); //modulus K
+
+    private int current; //current value in the sequence
+
+    public RandomNumGen() {
+        this.current = seed; // initializing w/ the seed value
+    }
+
+    public double nextRandom() {
+        current = ((multiplier * current) + increment) % modulus; //rule 1a: generating the next
+        return (double) current / modulus; //rule 1b: scaling to between 0 and 1
+    }
+
 }
