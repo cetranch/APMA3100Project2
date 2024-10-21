6878539208c9aa6c4656c3ddfd82ba7bc2460d20
cetranch <144961609+cetranch@users.noreply.github.com>
2024-10-21 15:47:20 -0400

    Update RandomNumGen.java
    fixed overflow issue causing negative values

--- RandomNumGen.java
+++ RandomNumGen.java
@@ -8,7 +8,7 @@ public class RandomNumGen {
     private static final int increment = 3517; // increment c
     private static final int modulus = (int) Math.pow(2, 17); //modulus K
 
-    private int current; //current value in the sequence
+    private long current; //current value in the sequence
 
     public RandomNumGen() {
         this.current = seed; // initializing w/ the seed value
